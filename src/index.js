7703ead69d09da3439bcdc73b324aa3c4105ba8f
Liz Goff <livingearthcreations@gmail.com>
2018-05-22 16:23:30 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+
+import { createStore, combineReducers, applyMiddleware, } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+const firstReducer = (state, action) => {
+    return {}; //return next state
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        firstReducer
+    }),
+    applyMiddleware(logger),
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
+registerServiceWorker();
